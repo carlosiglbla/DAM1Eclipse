6ee9223118badd3add0fcf3e767b6948fca7a39a
tirori <tirori@localhost>
2021-04-09 16:59:31 +0200

    Añadidos un par de comentarios

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -1,7 +1,15 @@
 package prueba1;
 
+/**
+ * Clase principal
+ *
+ */
 public class Principal {
 
+	/**
+	 * Main.
+	 * @param args argumentos
+	 */
 	public static void main(String []args) {
 		System.out.println("Hola mundo java-git");
 	}
