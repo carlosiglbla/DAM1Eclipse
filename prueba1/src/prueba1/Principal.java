4b1768e80b3806669a3adf0337d26eb68ebcd032
Usu2 Name <usu2@example.com>
2021-04-12 12:52:38 +0200

    Se añade una línea más.

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -14,6 +14,7 @@ public class Principal {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Nueva línea al mundo");
 		System.out.println("Otra línea");
+		System.out.println("Pruebas GIT. Se añade clase Cambio1");
 	}
 	
 }
