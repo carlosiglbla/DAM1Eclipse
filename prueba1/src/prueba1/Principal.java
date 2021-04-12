b836ac6f5899e2bcbd3ac45c1e8c0fdeb72f5eaf
Carlos Iglesias <carlosiglbla>
2021-04-12 11:23:18 +0200

    Subido dándole a commit and push

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -13,6 +13,7 @@ public class Principal {
 	public static void main(String []args) {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Nueva línea al mundo");
+		System.out.println("Otra línea");
 	}
 	
 }
